4f631c8cfff1df7b568515a783a5570dfb04e358
Ryan <164314230+Ryan-GNIT@users.noreply.github.com>
2025-09-14 15:33:44 +0400

    update axios header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import AdminUsers from "./Components/AdminUsers";
 import Consumers from "./Components/Consumers";
 import ContactSupport from "./Components/ContactSupport";
+import Header from "./Components/Header";
 import HomePage from "./Components/HomePage";
 import LicenseVerification from "./Components/LicenseVerification";
 import LoginPage from "./Components/Login/LoginPage";
 import Merchants from "./Components/Merchants";
 import React from "react";
 import Test from "./Components/Test";
+import VerticalNav from "./Components/VerticalNav";
+import axios from "axios";
+
+// Set a header specifically for POST requests
+axios.defaults.headers.post["ngrok-skip-browser-warning"] = "1";
 
 const App = () => {
   return (
